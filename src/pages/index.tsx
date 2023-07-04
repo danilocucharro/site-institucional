c91b79c7fdb199cd10fcd189422c967dbe1d2beb
danilocucharro <danicucharro@outlook.com>
2023-07-04 16:59:20 -0300

    base html da pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
-
+import Head from 'next/head'
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <div>
-      <h1>HELLO WORLD</h1>
-    </div>
+    <>
+      <Head>
+        <title>Apaixonado por tecnologia - Sujeito Programador</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.containerHeader}>
+          <section className={styles.ctaText}>
+            <h1>Levando você ao próximo nivel!</h1>
+            <span>Uma plataforma com cursos que vão do zero até o profissional na pratica, direto ao ponto aplicando oque usamos no mercado de trabalho. 👋</span>
+            <a>
+              <button>
+                COMEÇAR AGORA!
+              </button>
+            </a>
+          </section>
+
+          <img src="/images/banner-conteudos.png" alt="conteudos do curso" />
+        </div>
+      </main>
+    </>
   )
 }
